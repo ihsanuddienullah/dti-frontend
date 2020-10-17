d6829638ea9720f08a0b2ee480b2e565710e457c
ihsanuddienullah <ihsanuddienullah@gmail.com>
2020-10-17 12:15:40 +0700

    try react router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,24 @@ import React from 'react';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
+// import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom';
+// // import routes from './config/routes';
+
+// const isUserAuthenticated = false;
+
+// const PrivateRoute = ({ component: Component, ...rest }) => {
+//   return (
+//     <Route
+//       {...rest}
+//       render={() => {
+//         if (isUserAuthenticated) {
+//           return <Component />;
+//         }
+//         return <Redirect to="/login" />;
+//       }}
+//     />
+//   );
+// };
 
 class App extends React.Component {
   render() {
